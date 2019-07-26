fc6b67adb453ef3ffd65967c192e687c7f34fef4
Administrator <admin@example.com>
2019-07-26 16:03:21 +0100

    tidy up before section

--- example.tf
+++ example.tf
@@ -10,34 +10,18 @@ resource "aws_instance" "example"{
     instance_type = var.instance_type
 }
 
-resource "aws_instance" "another_instance" {
-    ami = var.amis[var.region]
-    instance_type = var.instance_type
-}
+# resource "aws_instance" "another_instance" {
+#     ami = var.amis[var.region]
+#     instance_type = var.instance_type
+# }
 
 # resource "aws_eip" "another_eip" {
 #     instance = aws_instance.another_instance.id
 # }
 
-# resource "aws_eip" "ip" {
-#     instance = aws_instance.example.id
-# }
-
-resource "aws_eip_association" "eip_assoc" {
-    instance_id = aws_instance.example.id
-    allocation_id = aws_eip.example.id
+resource "aws_eip" "ip" {
+    instance = aws_instance.example.id
 }
 
-resource "aws_eip_association" "another_eip_assoc" {
-    instance_id = aws_instance.another_instance.id
-    allocation_id = aws_eip.another_example.id
-}
 
-resource "aws_eip" "example" {
-    vpc = true
-}
-
-resource "aws_eip" "another_example" {
-    vpc = true
-}
 
