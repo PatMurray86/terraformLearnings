9cd1ff16b7ad6819a6a591b37b51a01222e126ec
Administrator <admin@example.com>
2019-07-26 15:57:14 +0100

    so many questions

--- example.tf
+++ example.tf
@@ -15,11 +15,29 @@ resource "aws_instance" "another_instance" {
     instance_type = var.instance_type
 }
 
-resource "aws_eip" "another_eip" {
-    instance = aws_instance.another_instance.id
+# resource "aws_eip" "another_eip" {
+#     instance = aws_instance.another_instance.id
+# }
+
+# resource "aws_eip" "ip" {
+#     instance = aws_instance.example.id
+# }
+
+resource "aws_eip_association" "eip_assoc" {
+    instance_id = aws_instance.example.id
+    allocation_id = aws_eip.example.id
 }
 
-resource "aws_eip" "ip" {
-    instance = aws_instance.example.id
+resource "aws_eip_association" "another_eip_assoc" {
+    instance_id = aws_instance.another_instance.id
+    allocation_id = aws_eip.another_example.id
+}
+
+resource "aws_eip" "example" {
+    vpc = true
+}
+
+resource "aws_eip" "another_example" {
+    vpc = true
 }
 
