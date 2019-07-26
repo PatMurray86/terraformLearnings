49f5930baaa2714dd8ada1bea4ea57e93c8d6b97
Administrator <admin@example.com>
2019-07-26 13:46:18 +0100

    remove unneeded instance and eip

--- example.tf
+++ example.tf
@@ -10,15 +10,7 @@ resource "aws_instance" "example"{
     instance_type = "t2.micro"
 }
 
-resource "aws_instance" "another_instance" {
-    ami = "ami-7ad7c21e"
-    instance_type = "t2.micro"
-}
-
 resource "aws_eip" "ip" {
     instance = aws_instance.example.id
 }
 
-resource "aws_eip" "another_ip" {
-    instance = aws_instance.another_instance.id
-}
