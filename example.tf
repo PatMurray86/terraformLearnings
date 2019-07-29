28c28e2d09712446c730503246d0110f2b1d2e6c
Administrator <admin@example.com>
2019-07-29 11:04:45 +0100

    kinda clean state

--- example.tf
+++ example.tf
@@ -1,24 +1,23 @@
+terraform {
+  backend "remote" {
+    organization = "pacina"
 
+    workspaces {
+      name = "pacina-test"
+    }
+  }
+}
 
 provider "aws" {
-    profile = "default"
-    region = "eu-west-2"
+  profile = "default"
+  region  = "eu-west-2"
 }
 
-resource "aws_instance" "example"{
-    ami = "ami-0cbe2951c7cd54704"
-    instance_type = "t2.micro"
-}
-
-resource "aws_instance" "another_instance" {
-    ami = "ami-7ad7c21e"
-    instance_type = "t2.micro"
+resource "aws_instance" "example" {
+  ami           = "ami-0cbe2951c7cd54704"
+  instance_type = "t2.micro"
 }
 
 resource "aws_eip" "ip" {
-    instance = aws_instance.example.id
-}
-
-resource "aws_eip" "another_ip" {
-    instance = aws_instance.another_instance.id
+  instance = aws_instance.example.id
 }
